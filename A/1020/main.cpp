716998e3cdd6af824bbbbabe4cd0267d1d32a1a1
ParadoxZW <964730078@qq.com>
2020-06-24 23:25:13 +0800

    1020

--- A/1020/main.cpp
+++ A/1020/main.cpp
@@ -15,7 +15,32 @@ struct node {
     int f, l, r, k;
 };
 
-node tree[maxn];
+void build(int root, int left, int rght, int rt_idx) {
+    int new_root;
+    if () {
+        
+    }
+    for (int i = left; i < rght; i++) {
+        if (inor[i] == post[root]) {
+            new_root = i;
+            break;
+        }
+    }
+    int lson, rson;
+
+    node lnd = {.f = rt_idx, .k = post[losn]};
+    node rnd = {.f = rt_idx, .k = post[rosn]};
+    tree.push_back(lnd);
+    tree.push_back(rnd);
+    tree[rt_idx].l = tree.size() - 2;
+    tree[rt_idx].r = tree.size() - 1;
+
+    build(lson, left, new_root);
+    build(rson, new_root + 1, rght)
+}
+
+// node tree[maxn];
+vector<node> tree;
 int post[32];
 int inor[32];
 int N;
